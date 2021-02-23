19fe3107e13732e38b160bac07981c250a332a78
Sebastian Arlund Nørgaard <sebnorgaard@gmail.com>
2021-02-23 12:30:31 +0100

    Add .el file blueprint

---
+++ elwait.el
@@ -0,0 +1,46 @@
+;;; elwait.el --- Async/await with Emacs threads -*- lexical-binding: t -*-
+
+;; Copyright (C) 2021 Sebastian Arlund Nørgaard
+
+;; Author: Sebastian Arlund Nørgaard
+;; Keywords: async, await, emacs
+;; URL:
+;; Package-Version: 0
+;; Package-Requires: ((emacs "27.1"))
+
+;; This file is not part of GNU Emacs.
+
+;; This file is free software: you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation, either version 3 of the License, or
+;; (at your option) any later version.
+
+;; This file is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+
+;; You should have received a copy of the GNU General Public License
+;; along with this file.  If not, see <http://www.gnu.org/licenses/>.
+
+;;; Commentary:
+
+;; Placeholder.
+
+;;; Code:
+
+(defgroup elwait nil
+  "Customization group for elwait, and async/await library."
+  :group 'extensions
+  :group 'lisp
+  :prefix "elwait-")
+
+(provide 'elwait)
+
+;; Local Variables:
+;; coding: utf-8
+;; indent-tabs-mode: nil
+;; require-final-newline: t
+;; End:
+
+;;; elwait.el ends here
